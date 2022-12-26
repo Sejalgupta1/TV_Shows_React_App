4e64522b2e1cde805f0a0a6c0e22bc9a34b29dc2
Sejalgupta1 <95693894+Sejalgupta1@users.noreply.github.com>
2022-12-26 21:53:11 +0530

    TV Shows App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Details from "./cardDetails";
+export default function App() {
+  const [dat, setData] = useState("");
+  const [curr, setCurr] = useState("");
+  const [page, setPage] = useState(1);
+  const [prev, setPrev] = useState(true);
+  const [next, setNext] = useState(false);
+  const [togg, setTogg] = useState(false);
+  const [sent, setSent] = useState(null);
+  const img = "./stars/";
+  useEffect(() => {
+    axios
+      .get("https://api.tvmaze.com/shows")
+      .then((res) => JSON.stringify(res.data))
+      .then((data) => {
+        setData(JSON.parse(data));
+        setCurr(JSON.parse(data).slice(0, 10));
+      });
+  }, []);
 
-function App() {
+  const goto = (val) => {
+    const selected = curr[val];
+    setSent(selected);
+    setTogg(true);
+  };
+  const back = () => {
+    setTogg(false);
+  };
+  const move = (cond) => {
+    if (cond === "next" && page !== dat.length / 10) {
+      setCurr(dat.slice(page * 10, (page + 1) * 10));
+      setPage(page + 1);
+      setPrev(false);
+      if (page === dat.length / 10 - 1) {
+        setNext(true);
+      }
+    } else if (cond === "prev" && page !== 0) {
+      setCurr(dat.slice((page - 1) * 10, page * 10));
+      setPage(page - 1);
+      setNext(false);
+      if (page === 1) {
+        setPrev(true);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="inner_clr">
+    <div className={!togg ? "App" : ""}>
+      {!togg ? (
+        <div>
+          {" "}
+          <header></header>
+          <h1 id="dash">TV Shows App</h1>
+          <div className="card">
+            {Array.from(curr).map((item, index) => {
+              return (
+                <div
+                  className="inner"
+                  onClick={() => {
+                    goto(index);
+                  }}
+                >
+                  <img id="movieimg" src={item.image.medium} alt={item.name} />
+                  <img
+                    id="rating"
+                    src={img.concat(
+                      `${String(Math.floor(item.rating.average / 2))}.jpg`
+                    )}
+                    alt=""
+                  />
+                  <p id="name">{item.name}</p>
+                </div>
+              );
+            })}
+            <div className="btn">
+               <button disabled={prev} onClick={() => move("prev")}>
+                Preview
+              </button>
+              <button disabled={next} onClick={() => move("next")}>
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Details name={sent} value={back} />
+        </div>
+      )}
+    </div>
+    </div>
     </div>
   );
 }
-
-export default App;
